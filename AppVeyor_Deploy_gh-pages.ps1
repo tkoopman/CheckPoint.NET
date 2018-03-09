c3bc8d56d99d7d0bc65f69c38798e16f412a5903
Tim <tkoopman@users.noreply.github.com>
2018-03-09 12:26:46 +1000

    AppVeyor

--- AppVeyor_Deploy_gh-pages.ps1
+++ AppVeyor_Deploy_gh-pages.ps1
@@ -2,6 +2,7 @@ git config --global credential.helper store
 Add-Content "$env:USERPROFILE\.git-credentials" "https://$($env:access_token):x-oauth-basic@github.com`n"
 git config --global user.email tkoopman@users.noreply.github.com
 git config --global user.name "Tim Koopman"
+git config --global core.autocrlf true
 
 git clone --single-branch --branch gh-pages https://github.com/tkoopman/CheckPoint.NET.git C:\projects\gh-pages
 cd C:\projects\gh-pages\
