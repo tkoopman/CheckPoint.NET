fef48f2400c7ccb5822672ae07f1d4179cc3eae4
Tim <tkoopman@users.noreply.github.com>
2018-03-09 11:47:52 +1000

    AppVeyor

--- AppVeyor_Deploy_gh-pages.ps1
+++ AppVeyor_Deploy_gh-pages.ps1
@@ -1,11 +1,13 @@
-git clone --single-branch --branch gh-pages https://github.com/tkoopman/CheckPoint.NET.git C:\projects\gh-pages
-Remove-Item -Path C:\projects\gh-pages\* -Force -Recurse
-Get-ChildItem -Path C:\projects\checkPoint.net\docs\* -Recurse -File | Move-Item -Destination C:\projects\gh-pages\
-cd C:\projects\gh-pages\
 git config --global credential.helper store
-Add-Content "$env:USERPROFILE.git-credentials" "https://$($env:GitToken):x-oauth-basic@github.com`n"
+Add-Content "$env:USERPROFILE\.git-credentials" "https://$($env:access_token):x-oauth-basic@github.com`n"
 git config --global user.email tkoopman@users.noreply.github.com
-git config --global user.name "tkoopman"
+git config --global user.name "Tim Koopman"
+
+git clone --single-branch --branch gh-pages https://github.com/tkoopman/CheckPoint.NET.git C:\projects\gh-pages
+cd C:\projects\gh-pages\
+git rm -r *
+Get-ChildItem -Path C:\projects\checkpoint-net\docs\* -Recurse -File | Move-Item -Destination C:\projects\gh-pages\
+
 git add --all
-git commit --message="Version $($env:VERSION)" --author="Tim Koopman <tkoopman@users.noreply.github.com>"
+git commit --message="Version $($env:VERSION)"
 git push origin gh-pages
